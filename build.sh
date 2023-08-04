2fc43dd754b54104b62caeb674ef86bad8cd6898
ssy38 <26034714+ssy38@users.noreply.github.com>
2023-08-04 17:25:37 -0400

    update build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-pip3 install -r requirements.txt
+pip install --upgrade pip && pip install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
