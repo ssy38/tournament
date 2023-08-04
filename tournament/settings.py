39c9722b1530e2c694b504ff580038d2d9d337d4
ssy38 <26034714+ssy38@users.noreply.github.com>
2023-08-04 17:34:33 -0400

    update build script

--- tournament/settings.py
+++ tournament/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'tournament.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/tournament',
         conn_max_age=600
     )
 }
