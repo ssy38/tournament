b60035e6a07075d17d15b5424d413e7263930119
ssy38 <26034714+ssy38@users.noreply.github.com>
2023-08-04 17:04:30 -0400

    render production branch

--- frontend/src/components/Button.js
+++
@@ -1,7 +0,0 @@
-export default function Button(props) {
-    return (
-        <button className="flex items-center justify-center bg-slate-300 text-slate-950 rounded-xl py-4 px-12 text-sm w-fit transition ease-in-out hover:bg-slate-400 hover:scale-105 duration-500">
-            {props.text}
-        </button>
-    );
-}
