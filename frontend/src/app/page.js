f1cf16e778038d5871401b88308d2af834e41376
ssy38 <26034714+ssy38@users.noreply.github.com>
2023-08-04 19:09:07 -0400

    update link

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -16,7 +16,7 @@ export default async function Page() {
                 <Hero title="Generate interactive brackets" />
             </main>
             <div className="flex mt-10 justify-center">
-                <a href="https://bracket-maker-django.onrender.com/" target="_blank" rel="noreferrer">
+                <a href="https://bracket-maker-django.onrender.com/status/" target="_blank" rel="noreferrer">
                     <div className="flex text-center align-middle justify-between min-w-fit px-3 py-2 bg-slate-900 rounded-full">
                         {online ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
